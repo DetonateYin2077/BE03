4b5bcaafc1be23e008e6c842978b113edad96844
DetonateYin2077 <detonate.yin@gmail.com>
2022-11-13 16:09:38 +0800

    ADD

--- server.js
+++ server.js
@@ -1,64 +1,82 @@
-"use strict";
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose');
+const shortId= require('shortid');
+var isUrl= require('is-url');
+var bodyParser = require('body-parser');
+const shortid = require('shortid');
 
-var express = require("express");
-var mongo = require("mongodb");
-var mongoose = require("mongoose");
-var cors = require("cors");
-var bodyParser = require("body-parser");
-var { nanoid } = require("nanoid");
-var dns = require("dns");
-var UrlModel = require("./models/url-model");
-var urlPostHandler = require('./url-post-handler')
+//var urlencodedParser = bodyParser.urlencoded({ extended: false })
+main().catch(err => console.log(err));
+async function main(){
+  await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  console.log('connected to MongoDB');
+};
 
-var app = express();
-// Basic Configuration
-var port = process.env.PORT || 3000;
-
-/** this project needs a db !! **/
-
-mongoose.connect(process.env.DB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+const urlSchema = new mongoose.Schema({
+  orgUrl:String,
+  shortUrl:String
 });
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+const URL = mongoose.model('URL', urlSchema);
 
+// Basic Configuration
+const port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.use("/public", express.static(process.cwd() + "/public"));
+app.post('/api/shorturl', async (req,res)=>{
+  const newUrl = req.body.url;
+  const shortenedUrl= shortId.generate();
+  console.log(isUrl(newUrl));
+  
 
-app.get("/", function(req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+  if (!isUrl(newUrl)){
+    res.json({error:'invalid url'});
+  } else {
+    try{
+      let findOne = await URL.findOne({orjUrl:newUrl})
+      if(findOne) res.json({original_url:findOne.orgUrl, short_url:findOne.shortUrl});
+      else {
+        findOne= new URL({orgUrl:newUrl,shortUrl:shortenedUrl});
+        await findOne.save();
+        res.json({original_url:newUrl,short_url:shortenedUrl});
+      }
+    }
+    catch(err){
+      console.log(err);
+      res.json({error:'server error'});
+    }
+  }
+  
 });
 
-// your first API endpoint...
-app.get("/api/hello", function(req, res) {
-  res.json({ greeting: "hello API" });
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+app.get('/api/shorturl/:shorturl', async (req,res)=>{
+  try {
+    const findOne = await URL.findOne({shortUrl:req.params.shorturl});
+    
+    if(findOne){
+      console.log('found');
+      res.redirect(findOne.orgUrl);
+    } else res.json({error:'no URL found'});
+  } catch(err){
+    console.log('no such short URL');
+    res.json({error:'no such short URL'});
+  }
+});
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
-// GET form for entering a URL
-app.get("/api/shorturl/new", function(req, res) {
-  res.sendFile(process.cwd() + "/views/url-form.html");
-});
 
-// Receive POST request, create a record in database, return a JSON record
-app.post("/api/shorturl/new", urlPostHandler);
-
-// GET short url and redirect to original url
-app.get("/api/shorturl/:shortUrl", function(req, res) {
-  UrlModel.findOne({ shortUrl: req.params.shortUrl }, "url", function(
-    error,
-    url
-  ) {
-    if (error) return new Error(error);
-    res.redirect(url.url);
-  });
-});
 
 app.listen(port, function() {
-  console.log("Node.js listening ...");
+  console.log(`Listening on port ${port}`);
 });
